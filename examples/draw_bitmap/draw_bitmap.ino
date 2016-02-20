a21897cd3e55945f4c7aa12a972dff0f8bd29974
MuchiriJohn <muchiri.mwihaki@gmail.com>
2016-02-20 13:56:30 +0300

    update: draw bitmap icons demo

--- examples/draw_bitmap/draw_bitmap.ino
+++ examples/draw_bitmap/draw_bitmap.ino
@@ -37,53 +37,45 @@ void drawIcons()
   //draw icon 1
   tft.drawBitmap(20, 50, earphones, 48, 48, 288);
   tft.drawText("AUDIO", 25, 100);
-  delay(20);
   tft.penColor(255, 255, 0);
-  delay(10);
+  delay(30);
   //draw icon 2
   tft.drawBitmap(100, 50, bulb, 48, 48, 288);
   tft.drawText("BULB", 105, 100);
-  delay(20);
   tft.penColor(0, 255, 0);
-  delay(10);
+  delay(20);
   //draw icon 3
   tft.drawBitmap(180, 50, on_off, 48, 48, 288);
   tft.drawText("SWITCH", 180, 100);
-  delay(20);
   tft.penColor(255, 0, 0);
-  delay(10);
+  delay(30);
   //draw icon 4
   tft.drawBitmap(20, 130, colors, 48, 48, 288);
   tft.drawText("COLOUR", 20, 180);
-  delay(20);
   tft.penColor(0, 0, 255);
-  delay(10);
+  delay(20);
   //draw icon 5
   tft.drawBitmap(100, 130, code, 48, 48, 288);
   tft.drawText("CODE", 105, 180);
-  delay(20);
   tft.penColor(255, 0, 255);
-  delay(10);
+  delay(30);
   //draw icon 6
   tft.drawBitmap(180, 130, call, 48, 48, 288);
   tft.drawText("CALL", 185, 180);
-  delay(20);
   tft.penColor(255, 255, 255);
-  delay(10);
-  //draw icon 7
-  tft.drawBitmap(20, 210, phone, 48, 48, 288);
-  tft.drawText("PHONE", 25, 260);
   delay(20);
+  //draw icon 7
+  tft.drawBitmap(20, 210, message, 48, 48, 288);
+  tft.drawText("MESSAGE", 15, 260);
   tft.penColor(55, 255, 55);
-  delay(10);
+  delay(30);
   //draw icon 8
   tft.drawBitmap(100, 210, settings, 48, 48, 288);
   tft.drawText("SETTINGS", 90, 260);
-  delay(20);
   tft.penColor(0, 255, 255);
-  delay(10);
+  delay(20);
   //draw icon 9
-  tft.drawBitmap(180, 210, record, 48, 48, 288);
-  tft.drawText("RECORD", 180, 260);
+  tft.drawBitmap(180, 210, info, 48, 48, 288);
+  tft.drawText("INFO", 185, 260);
 }
 
